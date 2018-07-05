d105db3050b6b485e0bbbdc0519be1ad89ca6b95
Greg Brown <gsb5067@gmail.com>
2018-07-05 12:39:43 -0400

    added dialin

--- collectors/cisco-dial-out-mdt-async-client.py
+++ collectors/cisco-dial-out-mdt-async-client.py
@@ -1,21 +1,22 @@
 import json
-import logging
 from argparse import ArgumentParser
 from configparser import ConfigParser
 from tornado.tcpserver import TCPServer
 from tornado.iostream import StreamClosedError
+from tornado.queues import Queue
 from tornado import gen
 from tornado.ioloop import IOLoop
 from struct import Struct, unpack
 from telemetry_pb2 import Telemetry
-
+from requests import request
 
 class TelemetryTCPDialOutServer(TCPServer):
-    def __init__(self):
+    def __init__(self, queue):
         super().__init__()
         self.header_size = 12
         self.header_struct = Struct('>hhhhi')
         self._UNPACK_HEADER = self.header_struct.unpack
+        self.queue = queue
         
     @gen.coroutine
     def handle_stream(self, stream, address):
@@ -39,13 +40,37 @@ class TelemetryTCPDialOutServer(TCPServer):
                     while len(msg_data) < msg_length:
                         packet = yield stream.read_bytes(msg_length - len(msg_data))
                         msg_data += packet
-                    #json_data = json.loads(msg_data.decode("ascii"))
-                    #print(json_data)
+                    json_data = json.loads(msg_data.decode("ascii"))
+                    del json_data
+                    #yield self.queue.put(json_data)
+                    #url = f"http://web-ott-tsdb-server-1:9200/{json_data['encoding_path'].replace('/','-').lower()}"
+                    #headers = {'Content-Type': "application/json"}
+                    #response = request("HEAD", url, headers=headers)
+                    #if response.status_code == 404:
+                    #    if not request("PUT", url, headers=headers).status_code == 200:
+                    #        print("Error when creating index")
+                    #url = f"{url}/{json_data['node_id_str']}"
+                    #body = json_data['data_json']
+                    #print(url)
+                    #print(body)
+                    #if not request("POST", url, json=body).status_code == 201:
+                    #    print("ERROR in POST")
+                        
             except Exception as e:
                 print(e)
+                print('Closing Session')
                 stream.close()
 
-        
+
+@gen.coroutine
+def rest_request(queue):
+    while True:
+        item = yield queue.get()
+        try:
+            #print(item)
+            pass
+        finally:
+            queue.task_done()
                 
 def main():
     parser = ArgumentParser()
@@ -53,7 +78,8 @@ def main():
     args = parser.parse_args()
     config_parser = ConfigParser()
     config_parser.read(args.config)
-    tcp_server = TelemetryTCPDialOutServer()
+    queue = Queue()
+    tcp_server = TelemetryTCPDialOutServer(queue)
     for section in config_parser.sections():
         for port in json.loads(config_parser.get(section,"Ports")):
             if section == 'TCP':
@@ -63,6 +89,7 @@ def main():
             else:
                 pass
     tcp_server.start(0)
+    #IOLoop.current().add_callback(rest_request, queue)
     IOLoop.current().start()
 
 
