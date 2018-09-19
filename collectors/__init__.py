6c8e195e866cbf04728f7baefed9b771be243bea
Greg Brown <gsb5067@gmail.com>
2018-09-19 11:59:51 -0400

    added __init__.py and removed some database code

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -1,1 +1,2 @@
+from . import cisco-dial-out-mdt-async-client, cisco-grpc-dialin, cisco-opendaylight-collector-client
 __all__ = ["cisco-dial-out-mdt-async-client", "cisco-grpc-dialin", "cisco-opendaylight-collector-client"]
