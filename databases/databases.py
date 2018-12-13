aa883d8bb563335399ad52610e9b258efdaa7d80
Greg Brown <gsb5067@gmail.com>
2018-12-13 13:27:21 -0500

    cleaned up some code

--- databases/databases.py
+++ databases/databases.py
@@ -13,23 +13,6 @@ class DBUploader(object):
         pass
 
 
-class InfluxDBUploader(DBUploader):
-    def __init__(self, db_name):
-        self.db = db_name
-        self.url = f"http://{self.address}:{self.port}/write"
-        self.request_params = {"db": self.db, "u": 'admin', "p": 'admin'}
-        self.header = {'Connection': 'close'}
-
-    def upload_to_db(self, data):
-        """Upload data in Line Format to InfluxDB database, returns True if uploaded, otherwise False """
-        rest_response = requests.request("POST", self.url, headers=self.header, data=data, params=self.request_params)
-        status_code = rest_response.status_code
-        response = rest_response.content.decode("utf-8")
-        if not (status_code == requests.codes.ok) and not (status_code == requests.codes.no_content):
-            return False, response, status_code
-        return True, response, status_code
-
-
 
 class ElasticSearchUploader(DBUploader):
     def __init__(self, index_list, db_address='0.0.0.0', db_port=9200, lock=Manager().Lock()):
