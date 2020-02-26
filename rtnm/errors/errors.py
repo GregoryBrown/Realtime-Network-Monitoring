a785463091468330c1392f65bdfbd2012315953e
Greg <gsb5067@gmail.com>
2020-02-26 14:25:51 -0500

    removed some error classes

--- rtnm/errors/errors.py
+++ rtnm/errors/errors.py
@@ -12,34 +12,6 @@ class TelemetryTCPDialOutServerError(Error):
     pass
 
 
-class GetIndexListError(Error):
-    """Error while doing a Get request against ES"""
-
-    def __init__(self, code, response_json, message):
-        self.code = code
-        self.response = response_json
-        self.message = message
-
-
-class PostDataError(Error):
-    """Error Posting data to ES"""
-
-    def __init__(self, code, response_json, data, message):
-        self.code = code
-        self.response = response_json
-        self.data = data
-        self.message = message
-
-
-class PutIndexError(Error):
-    """Error Putting an index into ES"""
-
-    def __init__(self, code, response_json, index, message):
-        self.code = code
-        self.response = response_json
-        self.index = index
-        self.message = message
-
 
 class FormatDataError(Error):
     """Formatting data failed with error"""
